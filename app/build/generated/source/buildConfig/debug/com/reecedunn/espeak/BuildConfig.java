5d25b601263df71f1085ebe0002769aa07769866
Abbas Rezaey <abbasrezaey1@gmail.com>
2025-10-16 11:05:43 +0200

    Initial commit: project restructured into app folder with espeak-src integration

---
+++ app/build/generated/source/buildConfig/debug/com/reecedunn/espeak/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.reecedunn.espeak;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.reecedunn.espeak";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 22;
+  public static final String VERSION_NAME = "1.52.0";
+}
